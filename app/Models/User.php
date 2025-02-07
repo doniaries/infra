1f019fa009fdd7dac928ef8a23b51a3c7f08b106
doni <doniaries@gmail.com>
2025-02-07 12:26:06 +0700

    fix shield

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\HasDatabaseNotifications;
 use Illuminate\Notifications\Notifiable;
@@ -37,5 +39,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessTenant(Model $tenant): bool
+    {
+        return $this->teams->contains($tenant);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Team;
+use App\Models\Jabatan;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::transaction(function () {
+            // Get Team Kominfo
+
+
+            // Super Admin
+            $superAdmin = User::create([
+                'name' => 'DON BORLAND',
+                'email' => 'superadmin@gmail.com',
+                'password' => bcrypt('password'),
+                // 'bidang_id' => $bidangTI->id,
+                // 'jabatan_id' => $jabatanStaff->id,
+                // 'is_active' => true,
+            ]);
+            $superAdmin->assignRole('super_admin');
+
+            //     // Admin Kominfo
+            //     $adminKominfo = User::create([
+            //         'name' => 'Muharis',
+            //         'email' => 'adminkominfo@gmail.com',
+            //         'password' => bcrypt('password'),
+            //         'bidang_id' => $bidangSekretariat->id,
+            //         'jabatan_id' => $jabatanbendaharaPengelolaBarang->id,
+            //         'nip' => '123453453535',
+            //         'is_active' => true,
+            //     ]);
+
+            //     $adminKominfo->assignRole('admin_opd');
+            //     $adminKominfo->teams()->attach($teamKominfo->id);
+
+            //     // User Bidang TI
+            //     $userBidangTI = User::create([
+            //         'name' => 'User Bidang TI',
+            //         'email' => 'userbidangti@gmail.com',
+            //         'password' => bcrypt('password'),
+            //         'bidang_id' => $bidangTI->id,
+            //         'jabatan_id' => $jabatanStaff->id,
+            //         'nip' => '12345634343',
+            //         'is_active' => true,
+            //     ]);
+
+            //     $userKepalaDinas = User::create([
+            //         'name' => 'DAVID RINALDO, SSTP',
+            //         'email' => 'userkepaladinas@gmail.com',
+            //         'password' => bcrypt('password'),
+            //         'bidang_id' => $bidangSekretariat->id,
+            //         'jabatan_id' => $jabatanKadis->id,
+            //         'nip' => '123424234324',
+            //         'is_active' => true,
+            //     ]);
+
+            //     $userKepalaDinas->assignRole('user_bidang');
+            //     $userKepalaDinas->teams()->attach($teamKominfo->id);
+
+            //     $userBidangTI->assignRole('user_bidang');
+            //     $userBidangTI->teams()->attach($teamKominfo->id);
+
+            //     // User Bidang Sekretariat
+            //     $userBidangSekretariat = User::create([
+            //         'name' => 'User Bidang Sekretariat',
+            //         'email' => 'userbidangsekretariat@gmail.com',
+            //         'password' => bcrypt('password'),
+            //         'bidang_id' => $bidangSekretariat->id,
+            //         'jabatan_id' => $jabatanStaff->id,
+            //         'is_active' => true,
+            //     ]);
+            //     $userBidangSekretariat->assignRole('user_bidang');
+            //     $userBidangSekretariat->teams()->attach($teamKominfo->id);
+
+            //     // Attach super admin ke semua team
+            //     Team::all()->each(fn($team) => $superAdmin->teams()->attach($team->id));
+        });
+    }
+}
