a56b4db03d7877fe119281208d51fd31b67bbbc7
doni <doniaries@gmail.com>
2025-02-07 10:49:03 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,16 @@ Route::get('/', function () {
 // Route::get('/', Listlaporan::class);
 
 Route::get('/laporform.php', ListLaporan::class);
+
+// Route::get('/', function () {
+//     return redirect('/admin/login');
+// });
+
+// Tangkap semua route yang tidak ditemukan
+Route::fallback(function () {
+    return redirect('/admin/login')->with('error', 'Akses ditolak! Silakan login terlebih dahulu.');
+});
+
+Route::get('/login', function () {
+    return redirect('/admin/login');
+})->name('login');
