18b46ff777d418fd62d39f86e070c97d7abf9a50
doni <doniaries@gmail.com>
2025-07-10 04:22:22 +0700

    dfdf

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,26 +9,26 @@ export default {
         "./resources/views/welcome.blade.php"
     ],
     plugins: [
-        require('tailwind-typewriter')({
-            wordsets: {
-                hero: {
-                    words: ['Ada Gangguan Jaringan?', 'Butuh Konsultasi Teknis?', 'Laporkan Sekarang!'],
-                    repeat: -1,
-                    writeSpeed: 0.1,    // Faster typing (lower is faster)
-                    eraseSpeed: 0.05,   // Faster erasing (lower is faster)
-                    delay: 0.5,         // Shorter initial delay
-                    pauseBetween: 1,    // Pause between words (in seconds)
-                    caret: '|',
-                    caretClassName: 'caret',
-                    lineBreak: false
-                }
-            },
-            // Global options
-            loop: true,
-            deleteSpeed: 0.05,
-            pauseTime: 1,
-            nextWordDelay: 0,
-            startDelay: 0.5
-        })
+        // require('tailwind-typewriter')({
+        //     wordsets: {
+        //         hero: {
+        //             words: ['Ada Gangguan Jaringan?', 'Butuh Konsultasi Teknis?', 'Laporkan Sekarang!'],
+        //             repeat: -1,
+        //             writeSpeed: 0.1,    // Faster typing (lower is faster)
+        //             eraseSpeed: 0.05,   // Faster erasing (lower is faster)
+        //             delay: 0.5,         // Shorter initial delay
+        //             pauseBetween: 1,    // Pause between words (in seconds)
+        //             caret: '|',
+        //             caretClassName: 'caret',
+        //             lineBreak: false
+        //         }
+        //     },
+        //     // Global options
+        //     loop: true,
+        //     deleteSpeed: 0.05,
+        //     pauseTime: 1,
+        //     nextWordDelay: 0,
+        //     startDelay: 0.5
+        // })
     ],
 };
